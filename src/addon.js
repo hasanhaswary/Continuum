a5ecc346c995ae0fa0470748542eeeac60e6bc35
Hasan Haswary <88339205+hasanhaswary@users.noreply.github.com>
2026-06-10 16:18:28 +0200

    chore(addon): added logo and description

--- src/addon.js
+++ src/addon.js
@@ -4,6 +4,8 @@ const builder = new addonBuilder({
     id: "com.hasan.continuum",
     version: "1.0.0",
     name: "Continuum",
+    description: "Fast streaming via multiple http providers",
+    logo: "utils\\logo.png",
     resources: ["stream"],
     types: ["movie", "series"],
     catalogs: []
