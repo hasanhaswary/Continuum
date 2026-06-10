656cfe4e8c7ff491cfcccf34536689ffffecfdc7
Hasan Haswary <88339205+hasanhaswary@users.noreply.github.com>
2026-06-10 16:05:57 +0200

    perf(provider): improved provider loading, manual to automatic

--- providers/index.js
+++ providers/index.js
@@ -1,11 +1,32 @@
-const vidfast =
-    require("./vidfastProvider");
+const fs =
+    require("fs");
 
-const vidsrc =
-    require("./vidsrcProvider");
+const path =
+    require("path");
 
-    
-module.exports = [
-    vidfast,
-    vidsrc,
-];
+const providers = [];
+
+const files =
+    fs.readdirSync(__dirname);
+
+for (const file of files) {
+
+    if (
+        file === "index.js" ||
+        !file.endsWith(".js")
+    ) {
+        continue;
+    }
+
+    providers.push(
+        require(
+            path.join(
+                __dirname,
+                file
+            )
+        )
+    );
+}
+
+module.exports =
+    providers;
